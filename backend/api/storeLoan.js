4b4016129ba10e6ad9cc893dba246efbcd5e358e
Kevin Mora Pais <kevin.mora@globant.com>
2020-12-22 15:23:30 -0300

    Same user with pending loan

--- backend/api/storeLoan.js
+++ backend/api/storeLoan.js
@@ -82,7 +82,7 @@ module.exports = async (req, res) => {
                 })
             }
         } catch (err) {
-            return res.status(500).json({ error: console.log(err) })
+            return res.status(500).json({ error: console.log("Test") })
         }
     }
 
