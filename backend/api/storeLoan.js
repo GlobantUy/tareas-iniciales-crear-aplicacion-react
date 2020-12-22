8471d645b3cead96502faa9e53341061ad680f7f
Kevin Mora Pais <kevin.mora@globant.com>
2020-12-22 16:24:46 -0300

    Amount verification

--- backend/api/storeLoan.js
+++ backend/api/storeLoan.js
@@ -94,7 +94,7 @@ module.exports = async (req, res) => {
 
                     })
                 }
-            }
+            
         } catch (err) {
             return res.status(500).json({ error: console.log("Test") })
         }
