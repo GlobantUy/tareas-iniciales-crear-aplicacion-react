2acc59c6a94536c8bd5f777eb916d87965703c97
Kevin Mora Pais <kevin.mora@globant.com>
2020-12-22 16:00:05 -0300

    Email verification

--- backend/api/storeLoan.js
+++ backend/api/storeLoan.js
@@ -14,66 +14,81 @@ module.exports = async (req, res) => {
 
     const db = await connectToDatabase();
     const collectionT = await db.collection("loans");
+    const collectionU = await db.collection("users");
     if (req.method === 'OPTIONS') {
         return res.status(200).send('ok');
     }
     if (req.method === 'POST') {
         try {
-            loanSearch = await collectionT.find({ userName: req.body.email }).toArray();
-
-            for (i = 0; i < loanSearch.length; i++) {
-                if (loanSearch[i].state == undefined) {
-                    conf = false
-                }
-            }
-
-            if (conf == true) {
-                const newLoan = new Loan({
-
-                    userName: req.body.email,
-                    amount: req.body.amount,
-                    date: cDate,
-                    currency: req.body.currency,
-                    payments: req.body.payments,
-                    state: undefined,
-                    _id: req.body.email + cDate,
-                    stateDate: cDate
-                })
-                try {
-
-                    db.collection("loans").insertOne(newLoan)
-
-                    return res.json({
-                        _links: {
-                            self: {
-                                href: 'https://vercelworking-ej6t36ecv.vercel.app/api/storeLoan'
-                            }
-                        },
-                        message: "Mabye it worked"
-                    })
-
-                } catch {
-                    return res.json({
-                        _links: {
-                            self: {
-                                href: 'https://vercelworking-ej6t36ecv.vercel.app/api/storeLoan'
-                            }
-                        },
-                        message: "Storage fail"
-
-                    })
-                }
-
-            } else {
+            userSearch = await collectionU.find({ email: req.body.email }).toArray();
+            if (userSearch.lenght != 1) {
                 return res.json({
                     _links: {
                         self: {
                             href: 'https://vercelworking-ej6t36ecv.vercel.app/api/storeLoan'
                         }
                     },
-                    message: "The user already has a loan pending approval"
-
+                    message: "Provided email does not belong to a registered user"
                 })
+            } else {
+                loanSearch = await collectionT.find({ userName: req.body.email }).toArray();
+
+
+                for (i = 0; i < loanSearch.length; i++) {
+                    if (loanSearch[i].state == undefined) {
+                        conf = false
+                    }
+                }
+
+                if (conf == true && req.body.email != undefined) {
+
+                    const newLoan = new Loan({
+
+                        userName: req.body.email,
+                        amount: req.body.amount,
+                        date: cDate,
+                        currency: req.body.currency,
+                        payments: req.body.payments,
+                        state: undefined,
+                        _id: req.body.email + cDate,
+                        stateDate: cDate
+                    })
+                    try {
+
+                        db.collection("loans").insertOne(newLoan)
+
+                        return res.json({
+                            _links: {
+                                self: {
+                                    href: 'https://vercelworking-ej6t36ecv.vercel.app/api/storeLoan'
+                                }
+                            },
+                            message: "Mabye it worked"
+                        })
+
+                    } catch {
+                        return res.json({
+                            _links: {
+                                self: {
+                                    href: 'https://vercelworking-ej6t36ecv.vercel.app/api/storeLoan'
+                                }
+                            },
+                            message: "Storage fail"
+
+                        })
+                    }
+
+                } else {
+                    return res.json({
+                        _links: {
+                            self: {
+                                href: 'https://vercelworking-ej6t36ecv.vercel.app/api/storeLoan'
+                            }
+                        },
+                        message: "The user already has a loan pending approval"
+
+                    })
+                }
             }
         } catch (err) {
             return res.status(500).json({ error: console.log("Test") })
