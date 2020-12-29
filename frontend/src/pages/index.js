d790ff37f30197952fc6e83e85e7f4e6caed3f4b
Alejandro Gonzalez Gonzalez <alejandro.gonzalez04@globant.com>
2020-12-29 16:38:02 -0300

    Arreglando error de header

--- frontend/src/pages/index.js
+++ frontend/src/pages/index.js
@@ -1,72 +1,17 @@
-
 import SimLoan from '../components/formSimLoan'
-import React, { Component } from 'react';
-
+import Header from '../components/Header'
 <style>
   @import url('https://fonts.googleapis.com/css2?family=Roboto&display=swap');
 </style>
-  const userLogin= ()=> {
-    try {
-    let btnHeader = document.querySelector(".btnHeader");
-    let user = document.querySelector(".User");
-      if (sessionStorage.getItem('Usuario-Values')){
-        btnHeader.style.display= 'none';
-        user.style.display= 'block';
-      } else {
-        btnHeader.style.display= 'block';
-        user.style.display= 'none';
-      }
+export default function Home() {
+  return (
+    <div>
 
-    } catch (error) {
-      console.log(error)
-    }
-    
-  }
-  
- class header extends React.Component{
-   constructor(props){
-     super(props)
-     this.state = {
-       email: '',
-       password:''
-     };
-   }
-   changeState (){
-     try {
-       let email = JSON.parse(sessionStorage.getItem('Usuario-Values')).email
-      this.setState({
-        email: email
-       })
-     } catch (error) {
-       
-     }
-    
-   }
-   
-   render(){
-    userLogin();
-    this.changeState()
-    return (
-      <div>
-        <header className="header">
-          <img className="logoheader" src="/logo.jpg" />
-          <a href="http://localhost:3000/ingreso" ><button className="btnHeader" type="submit"> Ingresar</button></a>
-       
-          <div className='User'>
-            <span id='user-name' >{this.state.email}</span>
-            <img className="imgUser" src="/Frame.png"/>
-          </div>
-  
-        </header>
-  
-        <SimLoan />
-          <footer>
-  
-          </footer>
-      </div>
-    )
-    
-   }
- }
- export default header;
+      <Header />      
+      <SimLoan />
+        <footer>
 
+        </footer>
+    </div>
+  )
+}
