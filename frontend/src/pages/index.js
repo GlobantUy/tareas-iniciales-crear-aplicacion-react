9eba87143e4fc580208f70234f741d66d9b04703
Alejandro Gonzalez Gonzalez <alejandro.gonzalez04@globant.com>
2020-12-29 15:39:58 -0300

    Mostrando el nombre del usuario del storage y arreglando problemas con el css

--- frontend/src/pages/index.js
+++ frontend/src/pages/index.js
@@ -1,17 +1,14 @@
 
 import SimLoan from '../components/formSimLoan'
+import React, { Component } from 'react';
+
 <style>
   @import url('https://fonts.googleapis.com/css2?family=Roboto&display=swap');
 </style>
-
-
   const userLogin= ()=> {
-
     try {
-      
     let btnHeader = document.querySelector(".btnHeader");
     let user = document.querySelector(".User");
-
       if (sessionStorage.getItem('Usuario-Values')){
         btnHeader.style.display= 'none';
         user.style.display= 'block';
@@ -23,28 +20,53 @@ import SimLoan from '../components/formSimLoan'
     } catch (error) {
       console.log(error)
     }
+    
   }
+  
+ class header extends React.Component{
+   constructor(props){
+     super(props)
+     this.state = {
+       email: '',
+       password:''
+     };
+   }
+   changeState (){
+     try {
+       let email = JSON.parse(sessionStorage.getItem('Usuario-Values')).email
+      this.setState({
+        email: email
+       })
+     } catch (error) {
+       
+     }
+    
+   }
+   
+   render(){
+    userLogin();
+    this.changeState()
+    return (
+      <div>
+        <header className="header">
+          <img className="logoheader" src="/logo.jpg" />
+          <a href="http://localhost:3000/ingreso" ><button className="btnHeader" type="submit"> Ingresar</button></a>
+       
+          <div className='User'>
+            <span id='user-name' >{this.state.email}</span>
+            <img className="imgUser" src="/Frame.png"/>
+          </div>
+  
+        </header>
+  
+        <SimLoan />
+          <footer>
+  
+          </footer>
+      </div>
+    )
+    
+   }
+ }
+ export default header;
 
-
-export default function Home() {
-  userLogin()
-  return (
-    <div>
-      <header className="header">
-        <img className="logoheader" src="/logo.jpg" />
-        <a href="http://localhost:3000/ingreso" ><button className="btnHeader" type="submit"> Ingresar</button></a>
-      
-        <div className='User'>
-          <span>Nombre</span>
-          <img className="imgUser" src="/Frame.png" />
-        </div>
-
-      </header>
-
-      <SimLoan />
-        <footer>
-
-        </footer>
-    </div>
-  )
-}
