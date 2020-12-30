9c3927bfc8739218f586fd5bc43506c71df35d44
Kevin Mora Pais <kevin.mora@globant.com>
2020-12-30 16:53:44 -0300

    Register endpoint email verification fix

--- backend/api/register.js
+++ backend/api/register.js
@@ -1,30 +1,43 @@
 import { connectToDatabase } from '../lib/database'
 const user = require('./models/user')
-const db = await connectToDatabase();
-const collection = await db.collection("users");
-let userSearch
 
 
-if (req.body.email != undefined) {
-    userSearch = await collection.find({ email: req.body.email }).toArray();
-    if (userSearch.length != 0) {
-        return res.json({
-            _links: {
-                self: {
-                    href: 'https://vercelworking-ej6t36ecv.vercel.app/api/storeLoan'
-                }
-            },
-            message: "Email belongs to an existing account",
-        })
-    } else {
+module.exports = async (req, res) => {
+
+    let userSearch
+    const db = await connectToDatabase();
+    const collectionU = await db.collection("users");
+
+    if (req.method === 'OPTIONS') {
+        return res.status(200).send('ok');
     }
-} else {
-    return res.json({
-        _links: {
-            self: {
-                href: 'https://vercelworking-ej6t36ecv.vercel.app/api/storeLoan'
+    if (req.method === 'POST') {
+        try {
+            if (req.body.email != undefined) {
+                userSearch = await collection.find({ email: req.body.email }).toArray();
+                if (userSearch.length != 0) {
+                    return res.json({
+                        _links: {
+                            self: {
+                                href: 'https://vercelworking-ej6t36ecv.vercel.app/api/storeLoan'
+                            }
+                        },
+                        message: "Email belongs to an existing account",
+                    })
+                } else {
+                }
+            } else {
+                return res.json({
+                    _links: {
+                        self: {
+                            href: 'https://vercelworking-ej6t36ecv.vercel.app/api/storeLoan'
+                        }
+                    },
+                    message: "Must provide an 'email' property"
+                })
             }
-        },
-        message: "Must provide an 'email' property"
-    })
+        } catch {
+
+        }
+    }
 }
