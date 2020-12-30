47058db90d7ed9e12a424a27d8a3b1740e7f1f16
Kevin Mora Pais <kevin.mora@globant.com>
2020-12-30 17:05:24 -0300

    userName property verification fix

--- backend/api/register.js
+++ backend/api/register.js
@@ -25,7 +25,7 @@ module.exports = async (req, res) => {
                         message: "Email belongs to an existing account",
                     })
                 } else {
-                    if (req.body.userName =! undefined){
+                    if (req.body.userName != undefined){
 
                     }else {
                         return res.json({
