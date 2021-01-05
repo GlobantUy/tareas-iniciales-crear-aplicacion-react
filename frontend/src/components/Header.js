0dd63015e783450684fb3a3240187149674511ab
Alejandro Gonzalez Gonzalez <alejandro.gonzalez04@globant.com>
2021-01-05 16:09:51 -0300

    Agregando Funcionalidad de Logout

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -4,46 +4,54 @@ import React, { Component } from 'react';
 <style>
   @import url('https://fonts.googleapis.com/css2?family=Roboto&display=swap');
 </style>
-  const userLogin= ()=> {
-    try {
+const userLogin = () => {
+  try {
     let btnHeader = document.querySelector(".btnHeader");
     let user = document.querySelector(".User");
-      if (sessionStorage.getItem('Usuario-Values')){
-        btnHeader.style.display= 'none';
-        user.style.display= 'block';
-      } else {
-        btnHeader.style.display= 'block';
-        user.style.display= 'none';
-      }
+    if (sessionStorage.getItem('Usuario-Values')) {
+      btnHeader.style.display = 'none';
+      user.style.display = 'block';
+    } else {
+      btnHeader.style.display = 'block';
+      user.style.display = 'none';
+    }
 
+  } catch (error) {
+    console.log(error)
+  }
+
+}
+
+class Header extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      email: '',
+      password: ''
+    };
+  }
+  changeState() {
+    try {
+      let email = JSON.parse(sessionStorage.getItem('Usuario-Values')).email
+      let arroba = email.indexOf("@");
+      this.setState({
+        email: email = email.substring(0, arroba)
+      })
+    } catch (error) {
+
+    }
+
+  }
+
+  logout() {
+    try {
+      sessionStorage.removeItem('Usuario-Values');
     } catch (error) {
       console.log(error)
     }
-    
   }
-  
- class Header extends React.Component{
-   constructor(props){
-     super(props)
-     this.state = {
-       email: '',
-       password:''
-     };
-   }
-   changeState (){
-     try {
-      let email = JSON.parse(sessionStorage.getItem('Usuario-Values')).email
-      let arroba = email.indexOf("@");
-     this.setState({
-       email: email = email.substring(0, arroba)
-       })
-     } catch (error) {
-       
-     }
-    
-   }
-   
-   render(){
+
+  render() {
     userLogin();
     this.changeState()
     return (
@@ -51,20 +59,27 @@ import React, { Component } from 'react';
         <header className="header">
           <img className="logoheader" src="/logo.jpg" />
           <a href="http://localhost:3000/ingreso" ><button className="btnHeader" type="submit"> Ingresar</button></a>
-       
+
           <div className='User'>
             <span id='user-name' >{this.state.email}</span>
-            <img className="imgUser" src="/Frame.png"/>
+            <div className="menu">
+            <img className="imgUser" src="/Frame.png" />
+              <ul>
+                <li>
+                  <a href='http://localhost:3000/ingreso' onClick={this.logout}>Log out </a>
+                </li>
+              </ul>
+            </div>           
           </div>
-  
+
         </header>
-  
-          <footer>
-  
-          </footer>
+
+        <footer>
+
+        </footer>
       </div>
     )
-    
-   }
- }
- export default Header;
+
+  }
+}
+export default Header;
