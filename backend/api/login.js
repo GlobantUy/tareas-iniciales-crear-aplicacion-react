85ed097a825eb876e094bffbbce6a747d31dd65d
Kevin Mora Pais <kevin.mora@globant.com>
2021-01-12 14:16:54 -0300

    Obtaining URL from request

--- backend/api/login.js
+++ backend/api/login.js
@@ -20,7 +20,7 @@ module.exports = async (req, res) => {
         return res.json({
           _links: {
             self: {
-              href:  req.get('host')
+              href:  req.url
             }
           },
           found: 'false'
@@ -31,7 +31,7 @@ module.exports = async (req, res) => {
         return res.json({
           _links: {
             self: {
-              href: req.get('host')
+              href: req.url
             }
           },
           email: userSearch[0].email,
@@ -42,7 +42,7 @@ module.exports = async (req, res) => {
       return res.json({
         _links: {
           self: {
-            href:  req.get('host')
+            href:  req.url
           }
         },
         message: 'Internal error (005)'
