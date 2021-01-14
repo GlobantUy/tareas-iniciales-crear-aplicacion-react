e2a361d0a4fd45453533472cef1fd0bfb9162d44
Kevin Mora Pais <kevin.mora@globant.com>
2021-01-14 16:59:33 -0300

    Loan return

---
+++ backend/api/customerLoans.js
@@ -0,0 +1,86 @@
+import { connectToDatabase } from '../lib/database'
+
+module.exports = async (req, res) => {
+    let loanSearch
+    let userSearch
+
+    const db = await connectToDatabase()
+    const collectionT = await db.collection('loans')
+    const collectionU = await db.collection('users')
+    if (req.method === 'OPTIONS') {
+        return res.status(200).send('ok')
+    }
+    if (req.method === 'POST') {
+        try {
+            userSearch = await collectionU.find({ email: req.body.email }).toArray()
+            loanSearch = await collectionT.find({ email: req.body.email}).toArray()
+            let conf = true
+            try {
+                userSearch[0].email
+            } catch {
+                conf = false
+                console.log(req.body.email)
+                return res.status(404).json({
+                    _links: {
+                        self: {
+                            href: "https://" + req.headers.host + req.url
+                        }
+                    },
+                    message: 'User not found'
+
+                })
+            }
+
+            if (conf == true) {
+
+                try {
+                    loanSearch[0].userName
+                } catch (err) {
+                    conf = false
+                    return res.status(200).json({
+                        _links: {
+                            self: {
+                                href: "https://" + req.headers.host + req.url
+                            }
+                        },
+                        message: 'No loans found'
+
+                    })
+                }
+
+                if (conf == true) {
+                    return res.status(200).json({
+                        _links: {
+                            self: {
+                                href: "https://" + req.headers.host + req.url
+                            }
+                        },
+                        loans: loanSearch
+
+                    })
+                }
+
+            }
+        } catch (err) {
+            return res.status(500).json({
+                _links: {
+                    self: {
+                        href: "https://" + req.headers.host + req.url
+                    }
+                },
+                message: 'Internal error (003)'
+
+            })
+        }
+    } else if (req.method != 'OPTIONS') {
+        return res.status(405).json({
+            _links: {
+                self: {
+                    href: "https://" + req.headers.host + req.url
+                }
+            },
+            message: 'Invalid method:' + ' "' + req.method + '"'
+
+        })
+    }
+}
