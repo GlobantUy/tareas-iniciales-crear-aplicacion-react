85d4c94903ab18e1b81a2ff5f848740ffdf26bfc
m-santamaria <m.santamaria@globant.com>
2021-01-28 14:36:44 -0300

    add endpoint serverles

--- backend/api/controllers/index.js
+++ backend/api/controllers/index.js
@@ -3,6 +3,8 @@ const express = require('express');
 const app = express();
 
 const bodyParser = require('body-parser');
+const LoginService = require('../login');
+const RegisterService = require('../register');
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -10,6 +12,36 @@ app.use(bodyParser.urlencoded({ extended: true }));
 //  base url to test our API
 app.get('/', async (req, res) => {
    res.send("<h2>Welcome to the Prestamos API for STB serverless App By Anima, Powered by Globant - 2020-2021</h2>")
+});
+
+app.post('/api/login', async (req, res) => {
+  try {
+      let ls = await LoginService.login(req, res);
+      res.status(ls.status).json(ls);
+  } catch (error) {
+      res.status(500).json(error);
+  }
+ 
+});
+
+app.post('/api/register', async (req, res) => {
+  try {
+      let ls = await RegisterService.register(req, res);
+      res.status(ls.status).json(ls);
+  } catch (error) {
+      res.status(500).json(error);
+  }
+ 
+});
+
+app.post('/api/returnLoan', async (req, res) => {
+  try {
+      let ls = await ReturnService.returnLoan(req, res);
+      res.status(ls.status).json(ls);
+  } catch (error) {
+      res.status(500).json(error);
+  }
+ 
 });
 
 module.exports.handler = serverless(app);
