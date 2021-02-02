74505e1effc5bfd2f75d94b3fe9456d06286bf78
m-santamaria <m.santamaria@globant.com>
2021-02-02 17:12:29 -0300

    add nprogress

--- frontend/src/pages/_app.js
+++ frontend/src/pages/_app.js
@@ -1,7 +1,18 @@
+/*import Router from 'next/router';
+import NProgress from 'nprogress'; //nprogress module
+import 'nprogress/nprogress.css'; //styles of nprogress
+*/
+import NextNprogress from 'nextjs-progressbar';
 import '../styles/globals.css'
+//Binding events. 
+/*Router.events.on('routeChangeStart', () => NextNprogress.start()); 
+Router.events.on('routeChangeComplete', () => NextNprogress.done()); 
+Router.events.on('routeChangeError', () => NextNprogress. .done());*/
 
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+
+
+function MyApp({ Component, pageProps, NextNprogress }) {
+  return <Component {...pageProps} NextNprogress />
 }
 
 export default MyApp
