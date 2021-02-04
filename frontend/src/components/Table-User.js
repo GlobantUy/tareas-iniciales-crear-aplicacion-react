62f7987b9f09014c04d5efec52549f16fbd760f6
Andrew-MartinezH <72889964+Andrew-MartinezH@users.noreply.github.com>
2021-02-04 18:14:51 -0300

    add page and component

---
+++ frontend/src/components/Table-User.js
@@ -0,0 +1,23 @@
+import React, { Component } from 'react';
+import 'bootstrap/dist/css/bootstrap.css';
+import axios from 'axios';
+
+class TableUser extends Component {
+
+    constructor(props) {
+        super(props)
+
+        this.state = {}
+    }
+
+    render() {
+        return (
+            <div>
+                <center>
+                    <h1>En proceso...</h1>
+                </center>
+            </div>
+        )
+    }
+}
+export default TableUser
