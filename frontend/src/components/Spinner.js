7b579600d9ad0993a5848f291129ae5bfe7a0cf5
m-santamaria <m.santamaria@globant.com>
2021-02-10 15:57:42 -0300

    add css velo

--- frontend/src/components/Spinner.js
+++ frontend/src/components/Spinner.js
@@ -1,7 +1,9 @@
 import React from 'react';
 
     const LoadingSpinner = () => (
+      <div>
       <div className = "spinner">
+        </div>
         <div id="carga"></div>
       </div>
     );
