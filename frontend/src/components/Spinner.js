dc84bbb36210225e392426454a103ad32c45f1aa
[bruno-dossantos] <[bruno.dossantos@globant.com]>
2021-02-10 16:37:20 -0300

    useState and useEffect added

--- frontend/src/components/Spinner.js
+++ frontend/src/components/Spinner.js
@@ -1,13 +1,9 @@
 import React from 'react';
-import { faSpinner } from '@fortawesome/free-solid-svg-icons'
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 
-    const LoadingSpinner = () => (
-      <div>
-          <FontAwesomeIcon icon={faSpinner} />
-                  <center><h2>Cargando...</h2></center>
-      </div>
-    );
+const LoadingSpinner = () => (
+  <div>
 
-    export default LoadingSpinner;
-    
+  </div>
+);
+
+export default LoadingSpinner;
--- frontend/src/components/formLogin.js
+++ frontend/src/components/formLogin.js
@@ -1,4 +1,4 @@
-import React, { Component, } from 'react';
+import React, { Component } from 'react';
 import { Formik } from 'formik';
 import axios from 'axios';
 import LoadingSpinner from './Spinner';
@@ -23,6 +23,20 @@ class SimLogin extends Component {
             loading: false,
         }
     }
+
+    Tooltip() {
+        const [isTooltipVisible, setTooltipVisibility] = React.useState(false);
+
+        React.useEffect(() => {
+            setTooltipVisibility(true);
+        }, []);
+        return (
+            <div className="App">
+                {isTooltipVisible && <div>tooltip component</div>}
+            </div>
+        );
+    }
+
     redireccionar() {
         const volverSolicitar = JSON.parse(sessionStorage.getItem('volverAceptarpress'));
         if (volverSolicitar) {
@@ -30,7 +44,7 @@ class SimLogin extends Component {
             if (rol == "ADMIN") {
                 window.location.href = "/Tableadmin"
                 sessionStorage.setItem('volverAceptarpress', false);
-            }else{
+            } else {
                 window.location.href = "/Descuento"
                 sessionStorage.setItem('volverAceptarpress', false);
             }
@@ -63,50 +77,45 @@ class SimLogin extends Component {
 
     post(email, pass) {
         this.setState({ loading: true }, () => {
-        axios.post(URL, {
-            "email": email,
-            "passwd": pass,
-        },
-        )
-            .then(Response => {
-                console.log("post realizado correctamente", Response)
-                if (Response.data.found == undefined) {
-                    rol = Response.data.rol;
-                    this.quitarError()
-                    if (rol == "CUSTOMER") {
-                        console.log(rol)
-                        this.redireccionar()
+            axios.post(URL, {
+                "email": email,
+                "passwd": pass,
+            },
+            )
+                .then(Response => {
+                    console.log("post realizado correctamente", Response)
+                    if (Response.data.found == undefined) {
+                        rol = Response.data.rol;
+                        this.quitarError()
+                        if (rol == "CUSTOMER") {
+                            console.log(rol)
+                            this.redireccionar()
+                        } else {
+                            console.log(rol)
+                            axios.post(URLreturnpres, {
+                                "email": emaill
+                            }).then(res => {
+                                console.log(res.data.loans)
+                                if (res.data.loans == "No loans found.") {
+                                    sessionStorage.setItem('prestamosNull', true);
+                                    this.redireccionar()
+                                    //sessionStorage.setItem('prestamos', JSON.stringify(res.data.loans));
+                                } else {
+                                    sessionStorage.setItem('prestamosNull', false);
+                                    this.redireccionar()
+                                    //this.redireccionar()
+                                }
+                            })
+                        }
                     } else {
-                        console.log(rol)
-                        axios.post(URLreturnpres, {
-                            "email": emaill
-                        }).then(res => {
-                            console.log(res.data.loans)
-                            if (res.data.loans == "No loans found.") {
-                                sessionStorage.setItem('prestamosNull', true);
-                                this.redireccionar()
-                                //sessionStorage.setItem('prestamos', JSON.stringify(res.data.loans));
-                            } else {
-                                sessionStorage.setItem('prestamosNull', false);
-                                
-                                this.redireccionar()
-
-
-                                //this.redireccionar()
-                            }
-                        })
-
+                        console.log(Response.data.found)
+                        this.setState({ loading: false });
+                        this.mostrarError()
                     }
-                } else {
-                    console.log(Response.data.found)
-                    this.setState({loading:false});
-                    this.mostrarError()
-
-                }
-            })
-            .catch(error => {
-                console.log("Error al iniciar sesion", error)
-            });
+                })
+                .catch(error => {
+                    console.log("Error al iniciar sesion", error)
+                });
         })//fin state loading
     }
 
@@ -125,8 +134,8 @@ class SimLogin extends Component {
         const { loading } = this.state
         return (
             <div className="formLogin">
-            { loading ? <LoadingSpinner />: <div /> }
-            
+                { loading ? <LoadingSpinner /> : <div />}
+
                 <h1>Ingreso</h1>
                 <Formik
                     initialValues={{ email: '', password: '' }}
