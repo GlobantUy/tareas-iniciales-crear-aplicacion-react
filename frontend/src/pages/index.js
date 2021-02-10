7f8e462a1314a165048d8ac21a41e54c37d57bb7
Alejandro Gonzalez Gonzalez <alejandro.gonzalez04@globant.com>
2021-02-10 17:39:40 -0300

    Solucionado problema de Administrador sobre el home

--- frontend/src/pages/index.js
+++ frontend/src/pages/index.js
@@ -2,14 +2,62 @@ import SimLoan from '../components/formSimLoan'
 import Header from '../components/Header'
 
 export default function Home() {
-  return (
-    <div>
-      <Header />
-      <SimLoan />
 
-      <footer>
-
-      </footer>
-    </div>
-  )
+  try {
+    let rol= JSON.parse(sessionStorage.getItem('Usuario-Values')).role;
+    switch (rol) {
+      case 'ADMIN':
+        console.log('admin catch')
+        return (
+          <div>
+            <Header />
+              <h2 className='admin-text'>Bienvenido Administrador</h2>    
+            <footer>
+      
+            </footer>
+          </div>
+        )
+        break;
+      case 'CUSTOMER':
+        console.log('customer catch')
+        return (
+          <div>
+            <Header />
+            <SimLoan />
+      
+            <footer>
+      
+            </footer>
+          </div>
+        )        
+        break;
+      default:
+        console.log('default catch')
+        return (
+          <div>
+            <Header />
+            <SimLoan />
+      
+            <footer>
+      
+            </footer>
+          </div>
+        )
+        break;
+    }
+   
+  } catch (error) {
+    console.log('error catch')
+    return (
+      <div>
+        <Header />
+        <SimLoan />
+  
+        <footer>
+  
+        </footer>
+      </div>
+      
+    )
+  }
 }
